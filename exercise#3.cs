f5b2bff39f183efed3df5dea3a09a44dbf64c3df
likk-G <83183455+likk-G@users.noreply.github.com>
2023-04-09 13:37:19 +0400

    Add files via upload

---
+++ exercise#3.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+int[] arr = { 3, 5, 5, 5, 2, 2, 1, 7, 7, 7, 7, 8, 8, 2, 2, 2 };
+int max = 1, tmpMax, value = arr[0];
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    tmpMax = 1;
+    if (arr[i] == arr[i + 1])
+    {
+        while (i < arr.Length - 1 && arr[i + 1] == arr[i])
+        {
+            tmpMax++;
+            i++;
+        }
+    }
+    if (tmpMax > max)
+    {
+        max = tmpMax;
+        value = arr[i];
+    }
+}
+
+Console.WriteLine($"Maximum length: {max}, value: {value}");
